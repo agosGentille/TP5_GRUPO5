a788cd12bf16f7645928cb3441d906470cb0fd57
Joel <86167674+joelayllon@users.noreply.github.com>
2023-09-20 01:52:38 -0300

    Update Pelicula.java

--- TP5_Grupo5/src/TP5/Pelicula.java
+++ TP5_Grupo5/src/TP5/Pelicula.java
@@ -1,23 +1,33 @@
+
 package TP5;
 
 public class Pelicula {
-	private final int id;
-	private String nombre;
-	private static int contador = 0;
-	
-	public Pelicula(){
-		this.id = contador++;
-		this.nombre = "sin nombre";
-	};
-	
-	public Pelicula(int id, String nombre, String genero) {
-		super();
-		this.id = contador++;
-		this.nombre = nombre;
+	@Override
+	public String toString() {
+		return nombre + ", "+genero;
 	}
+	private int id;
+	private String nombre;
+	private Genero genero;
+	private static int cont = 1;
 
+	public Pelicula() {
+		this.id = cont;
+	}
+	
+	public Pelicula(String nombre, Genero genero) {
+		super();
+		this.id = cont;
+		cont++;
+		this.nombre = nombre;
+		this.genero = genero;
+	}
+	
 	public int getId() {
 		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
 	}
 	public String getNombre() {
 		return nombre;
@@ -25,13 +35,13 @@ public class Pelicula {
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
-
-
-	@Override
-	public String toString() {
-		return "Pelicula: " + id + "- " + nombre;
+	public Genero getGenero() {
+		return genero;
 	}
-	
-	
-
+	public void setGenero(Genero genero) {
+		this.genero = genero;
+	}
+	public int getCont() {
+		return cont;
+	}
 }
